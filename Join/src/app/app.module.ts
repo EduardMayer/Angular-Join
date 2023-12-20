4957ef3c86564f220ad17516ce0180fae214b373
Eduard Mayer <119446765+EduardMayer@users.noreply.github.com>
2023-12-21 00:03:13 +0100

    add some components

--- Join/src/app/app.module.ts
+++ Join/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IntroComponent } from './startscreen/intro/intro.component';
+import { LoginComponent } from './startscreen/login/login.component';
+import { SignupComponent } from './startscreen/signup/signup.component';
+import { LegalNoticeComponent } from './startscreen/legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './startscreen/privacy-policy/privacy-policy.component';
+import { StartscreenComponent } from './startscreen/startscreen.component';
+import { MainscreenComponent } from './mainscreen/mainscreen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    LoginComponent,
+    SignupComponent,
+    LegalNoticeComponent,
+    PrivacyPolicyComponent,
+    StartscreenComponent,
+    MainscreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
